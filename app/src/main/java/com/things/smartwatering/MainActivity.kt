4b2630c351e5e669d3c3cb82ee5ac7386a3d8fee
Alexander Smityuk <sanchosmit@gmail.com>
2018-03-05 21:34:17 +0200

    Init project

---
+++ app/src/main/java/com/things/smartwatering/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.things.smartwatering
+
+import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.hardware.SensorManager.DynamicSensorCallback
+import android.content.Intent
+
+private val TAG = MainActivity::class.java.simpleName
+
+class MainActivity : Activity() {
+    private lateinit var mSensorManager: SensorManager
+    private val mDynamicSensorCallback = object : DynamicSensorCallback() {
+        override fun onDynamicSensorConnected(sensor: Sensor) {
+            if (sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+                Log.i(TAG, "Temperature sensor connected")
+                mSensorEventListener = TemperaturePressureEventListener()
+                mSensorManager.registerListener(mSensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+            }
+        }
+    }
+    private lateinit var mSensorEventListener: TemperaturePressureEventListener
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startTemperaturePressureRequest()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopTemperaturePressureRequest()
+    }
+
+    private fun startTemperaturePressureRequest() {
+        this.startService(Intent(this, TemperaturePressureService::class.java))
+        mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        mSensorManager.registerDynamicSensorCallback(mDynamicSensorCallback)
+    }
+
+    private fun stopTemperaturePressureRequest() {
+        this.stopService(Intent(this, TemperaturePressureService::class.java))
+        mSensorManager.unregisterDynamicSensorCallback(mDynamicSensorCallback)
+        mSensorManager.unregisterListener(mSensorEventListener)
+    }
+
+    private inner class TemperaturePressureEventListener : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            Log.i(TAG, "sensor changed: " + event.values[0])
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+            Log.i(TAG, "sensor accuracy changed: " + accuracy)
+        }
+    }
+
+}
