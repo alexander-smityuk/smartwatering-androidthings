498516149da844bb72224f4ac1fc0ab11534951d
Alexander Smityuk <sanchosmit@gmail.com>
2018-05-28 12:57:53 +0300

    Add water status listener

--- app/src/main/java/com/things/smartwatering/MainActivity.kt
+++ app/src/main/java/com/things/smartwatering/MainActivity.kt
@@ -11,9 +11,14 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.things.smartwatering.driver.pump.Pump
 import com.things.smartwatering.driver.pump.WaterPump
 import com.things.smartwatering.model.DataInfo
+import com.things.smartwatering.model.Status
 import com.things.smartwatering.repository.FirebaseRepository
 import com.things.smartwatering.repository.FirebaseRepositoryImpl
 import com.things.smartwatering.service.SensorService
@@ -57,10 +62,19 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mFireBaseRepository = FirebaseRepositoryImpl()
-
         waterPump = WaterPump(PUMP_GPIO_PIN)
-        waterPump.water(15, TimeUnit.SECONDS)
+
+        mFireBaseRepository = FirebaseRepositoryImpl()
+        mFireBaseRepository.getStatusData(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val status = dataSnapshot.getValue(Status::class.java)
+                waterPump.water(status!!.waterStatus)
+            }
+
+            override fun onCancelled(error: DatabaseError?) {
+                Log.e("Firebase", error.toString())
+            }
+        })
 
         startSensorRequest()
         startAdvertising()
